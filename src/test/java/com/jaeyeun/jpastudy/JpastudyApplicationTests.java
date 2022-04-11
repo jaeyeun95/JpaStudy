1b630f8c294198ba807e67156fd9dad70ec329c3
jaeyeun <jaeyeun95@gmail.com>
2022-04-11 23:22:45 +0900

    프로젝트 생성

---
+++ src/test/java/com/jaeyeun/jpastudy/JpastudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaeyeun.jpastudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpastudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
