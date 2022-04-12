10fe98c72ffa910cce1944bb84dfc41227259a38
jaeyeun <jaeyeun95@gmail.com>
2022-04-13 00:01:37 +0900

    member @transactional 추가 및 게시판 엔티티 추가

---
+++ src/main/java/com/jaeyeun/jpastudy/board/BoardDTO.java
@@ -0,0 +1,13 @@
+package com.jaeyeun.jpastudy.board;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class BoardDTO {
+
+//    private
+}
--- src/main/java/com/jaeyeun/jpastudy/member/MemberService.java
+++ src/main/java/com/jaeyeun/jpastudy/member/MemberService.java
@@ -3,10 +3,12 @@ package com.jaeyeun.jpastudy.member;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor
+@Transactional
 public class MemberService {
 
     private final MemberRepository memberRepository;
